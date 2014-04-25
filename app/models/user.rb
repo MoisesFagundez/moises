60fc73643d49a1abd87e05ae88b5d93446c504b8
MarlonFagundez <marlon.fagundez@gamygame.com>
2014-04-25 16:33:11 -0430

    agregados provider y uid user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :provider, :uid
 end
