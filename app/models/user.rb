9d3018f2e7f5dfcbd6c62faafb03ebfba2903122
MarlonFagundez <marlon.fagundez@gamygame.com>
2014-04-28 10:38:46 -0430

    agregado bloque en el modelo user para integracion con google+

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :provider, :uid
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook, :google_oauth2, :linkedin]
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :provider, :uid, :social_data, :name
+
 
   def self.find_for_facebook_oauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
@@ -20,6 +23,25 @@ class User < ActiveRecord::Base
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+    if user
+      return user
+    else
+      registered_user = User.where(:email => access_token.info.email).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(name: data["name"],
+                           provider:access_token.provider,
+                           email: data["email"],
+                           uid: access_token.uid ,
+                           password: Devise.friendly_token[0,20],
+        )
       end
     end
   end
